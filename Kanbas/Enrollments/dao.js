47efae9cc30c9b8dbbd1abbf429fb019784baa3d
Andrew <tsai.an@northeastern.edu>
2024-12-06 23:58:08 -0500

    a6

--- Kanbas/Enrollments/dao.js
+++ Kanbas/Enrollments/dao.js
@@ -1,3 +1,4 @@
+import model from "./model.js";
 import Database from "../Database/index.js";
 
 export function findEnrollmentsForUser(userId) {
@@ -5,15 +6,17 @@ export function findEnrollmentsForUser(userId) {
   const enrolledCourses = enrollments.filter((enrollment) => enrollment.user === userId);
   return enrolledCourses;
 }
-
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  Database.enrollments.push({ _id: Date.now().toString(), user: userId, course: courseId });
-  return enrollments;
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function unenrollUserFromCourse(enrollmentId) {
-    const { enrollments } = Database;
-    Database.enrollments = enrollments.filter((enrollment) => enrollment._id !== enrollmentId);
-    return enrollments;
+export async function findUsersForCourse(courseId) {
+  const enrollments = await model.find({ course: courseId }).populate("user");
+  return enrollments.map((enrollment) => enrollment.user);
+}
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course });
+}
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
 }
